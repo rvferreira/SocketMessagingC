8e48af0c8f84d382ce97297aeebe17ee36a4a9d0
Raphael Ferreira <raphael.rvf@gmail.com>
2015-06-06 10:44:43 -0300

    hello world

---
+++ src/SocketMessagingC.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : SocketMessagingC.c
+ Author      : Raphael Ferreira e Jéssika Darambaris
+ Version     : 1.0
+ Copyright   : Use a vontz, mas conserve os créditos
+ Description : Server-Client message passing in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!");
+	return EXIT_SUCCESS;
+}
