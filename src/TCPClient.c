7bb298bc0b8788cd8a1944d6e44ef3ff28fda00b
Raphael Ferreira <raphael.rvf@gmail.com>
2015-07-10 00:43:22 -0300

    contact list bugfix

--- src/TCPClient.c
+++ src/TCPClient.c
@@ -14,93 +14,85 @@
 int sockClient;
 
 /* estabelece uma conexão com um servidor online */
-int establishedConnection(char ip[])
-{
-   struct hostent *host;
-   struct sockaddr_in server_addr;
-   ServerMessage recv_data;
+int establishedConnection(char ip[]) {
+    struct hostent *host;
+    struct sockaddr_in server_addr;
+    ServerMessage recv_data;
 
-   host = gethostbyname(ip);
-   
-    if (!strcmp(ip, "0")){
+    host = gethostbyname(ip);
+
+    if (!strcmp(ip, "0")) {
         return 1;
     }
-   /* cria uma variável do tipo sock */
-   if ((sockClient = socket(AF_INET, SOCK_STREAM, 0)) == -1)
-   {
-      perror("Error: failed function socket");
-      exit(1);
-   }
+    /* cria uma variável do tipo sock */
+    if ((sockClient = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        perror("Error: failed function socket");
+        exit(1);
+    }
 
-   server_addr.sin_family = AF_INET;
-   server_addr.sin_port = htons(DEFAULT_PORT);
-   server_addr.sin_addr = *((struct in_addr *)host->h_addr);
-   bzero(&(server_addr.sin_zero),8);
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(DEFAULT_PORT);
+    server_addr.sin_addr = *((struct in_addr *) host->h_addr);
+    bzero(&(server_addr.sin_zero), 8);
 
-   if (connect(sockClient, (struct sockaddr *)&server_addr, sizeof(struct sockaddr)) == -1)
-   {
-      perror("Server offline");
-      return 1;
-   }
+    if (connect(sockClient, (struct sockaddr *) &server_addr, sizeof(struct sockaddr)) == -1) {
+        perror("Server offline");
+        return 1;
+    }
 
-   recv(sockClient,(void *)&recv_data,sizeof(recv_data),0);
-   
-   // copia para myIP o valor que o server deu para a máquina cliente  
-   strcpy(myIP,recv_data.target);
-   myPort = recv_data.port;
-  
-   // mensagem emitida do servidor
-   printf("\n %s \n", recv_data.message);
-   getchar();
-    onlineUsers = malloc (sizeof(OnlineUser));
+    recv(sockClient, (void *) &recv_data, sizeof(recv_data), 0);
 
-  return 0;
+    // mensagem emitida do servidor
+    printf("\n %s \n", recv_data.message);
+    getchar();
+
+    return 0;
 
 }
 
-void closeConnectionServer(){
-   ServerMessage m;
-   strcpy(m.message,"close");
-   strcpy(m.origin, myIP);
-   /* avisa o servidor que fechou a conexão */ 
-   send(sockClient,(void *)&m,sizeof(m), 0);
-   close(sockClient);
+void closeConnectionServer() {
+    ServerMessage m;
+    strcpy(m.message, "close");
+    strcpy(m.origin, myIP);
+    /* avisa o servidor que fechou a conexão */
+    send(sockClient, (void *) &m, sizeof(m), 0);
+    close(sockClient);
 }
 
-void sendMessage(ServerMessage m){
-   ServerMessage reply;
-   
-   /* envia mensagem para o servidor */
-   if (send(sockClient,(void *)&m,sizeof(m), 0) == -1) {
-      printf("Error: Message \n");
-   }
+void sendMessage(ServerMessage m) {
+    ServerMessage reply;
 
-   /*servidor confirma o recebimento da mensagem */
-  recv(sockClient,(void *)&reply,sizeof(reply),0);
-  printf("\n %s \n",reply.message);
+    /* envia mensagem para o servidor */
+    if (send(sockClient, (void *) &m, sizeof(m), 0) == -1) {
+        printf("Error: Message \n");
+    }
+
+    /*servidor confirma o recebimento da mensagem */
+    recv(sockClient, (void *) &reply, sizeof(reply), 0);
+    printf("\n %s \n", reply.message);
 }
 
-void receiveMessage(){
-   ServerMessage reply;
-   
-   /* fica esperando uma mensagem do servidor */
-   recv(sockClient,(void *)&reply,sizeof(reply),0);
-   
-   /* servidor enviando a lista de contatos */
-   if (!strcmp(reply.message,"broadcast_list_online")){
-        send(sockClient,(void *)&reply,sizeof(reply),0);
+void receiveMessage() {
+    ServerMessage reply;
+
+    /* fica esperando uma mensagem do servidor */
+    recv(sockClient, (void *) &reply, sizeof(reply), 0);
+
+    /* servidor enviando a lista de contatos */
+    if (!strcmp(reply.message, "broadcast_list_online")) {
+        send(sockClient, (void *) &reply, sizeof(reply), 0);
 
         sleep(1);
-        recv(sockClient,&nOnlineUsers,sizeof(int),0);
-        send(sockClient,"OK",sizeof("OK"),0);
+        recv(sockClient, &nOnlineUsers, sizeof(int), 0);
+        send(sockClient, "OK", sizeof("OK"), 0);
         sleep(1);
-        
-        onlineUsers = realloc (NULL,sizeof(OnlineUser)*(nOnlineUsers+1));
-        
-        recv(sockClient, onlineUsers,sizeof(onlineUsers),0);
-        send(sockClient,"OK",sizeof("OK"),0);
-        
-   }
 
-   return;
+        onlineUsers = realloc(NULL, sizeof(OnlineUser) * (nOnlineUsers + 1));
+
+        recv(sockClient, onlineUsers, sizeof(onlineUsers), 0);
+        send(sockClient, "OK", sizeof("OK"), 0);
+
+    }
+
+    return;
 }
